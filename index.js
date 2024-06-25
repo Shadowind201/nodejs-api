c9b1613a3890374437b74728ae9ea11beb71df4f
Shadowind201 <enzocarnier@gmail.com>
2024-06-24 21:02:15 -0300

    .

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express()
 
-app.get("/", function(req,res ){
-    res.send("Hello Word")
+const herois = ["homem aranha", "hulk", "ms. marvel"]
+
+// ENDPOINT listagem de todos os herios(READ ALL => [GET])
+app.get("/heroes", function(rep, res){
+    res.send("Lista de todos os herois")
 })
 
-app.get("/ptbr", function(req,res ){
-    res.send("Olá mundo")
+app.get("/heroes/:id", function(req, res){
+    //acessar o parametro de rota ID
+    const id = req.params.id
+
+    //pego o item no array
+    const umheroi = herois[id]
+
+    // eu envio o item encontrado como resposta
+    res.send(umheroi)
 })
 
 app.listen(3000)
