c0014f5167bc1a50014813246c2a83b7ad0ac690
Shadowind201 <enzocarnier@gmail.com>
2024-06-24 21:19:03 -0300

    post

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.get("/heroes", function(rep, res){
     res.send("Lista de todos os herois")
 })
 
+
+//ENDPOINT listagem de UM  heroi (READ SINGLE)
 app.get("/heroes/:id", function(req, res){
     //acessar o parametro de rota ID
     const id = req.params.id
@@ -17,6 +19,14 @@ app.get("/heroes/:id", function(req, res){
 
     // eu envio o item encontrado como resposta
     res.send(umheroi)
+})
+
+//todo o corpo da requisiçõa virá como JSON
+app.use(express.json())
+
+//ENDPOINT  de criação (CREATE => [POST])
+app.post("/heroes", function(req, res){
+    res.send(req.body)
 })
 
 app.listen(3000)
